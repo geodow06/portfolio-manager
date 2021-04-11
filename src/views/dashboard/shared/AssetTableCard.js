e4b6d4a7e3c89e109b4aec2ece0831255f608f44
George <georgedowne06@gmail.com>
2021-04-11 18:50:27 +0100

    Generating table rows from asset list

--- src/views/dashboard/shared/AssetTableCard.js
+++ src/views/dashboard/shared/AssetTableCard.js
@@ -9,6 +9,26 @@ import {
 } from "@material-ui/core";
 
 const AssetTableCard = () => {
+
+    // TODO get portfolio assets from API
+    const assetList = [
+        { 
+            name: "Bitcoin",
+            balance: "$1.00 1 BTC",
+            allocation: "10%"
+        },
+        { 
+            name: "Stellar Lumens",
+            balance: "$2.00 1 XLM",
+            allocation: "20%"
+        },
+        { 
+            name: "Gather",
+            balance: "$7 1 GTH",
+            allocation: "70%"
+        },
+    ];
+
     return(
         <Card elevation={6} className="px-24 pt-20 py-16 mb-24">
             <div className="card-title ml-12">Your Assets</div>
@@ -20,11 +40,13 @@ const AssetTableCard = () => {
                         <TableCell>Allocation</TableCell>
                     </TableHead>
                     <TableBody>
-                        <TableRow>
-                            <TableCell>asset name</TableCell>
-                            <TableCell>balance value</TableCell>
-                            <TableCell>allocation percentage</TableCell>
-                        </TableRow>
+                        {assetList.map((asset, index) => 
+                            <TableRow key={index}>
+                                <TableCell>{asset.name}</TableCell>
+                                <TableCell>{asset.balance}</TableCell>
+                                <TableCell>{asset.allocation}</TableCell>
+                            </TableRow>
+                        )}
                     </TableBody>
                 </Table>
             </div>
