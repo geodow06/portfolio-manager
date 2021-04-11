787c0595e19a6ae6ce1f9438ce13c195753aff74
George <georgedowne06@gmail.com>
2021-04-11 18:44:20 +0100

    Adding base table structure

--- src/views/dashboard/shared/AssetTableCard.js
+++ src/views/dashboard/shared/AssetTableCard.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { Card } from "@material-ui/core";
+import { 
+    Card, 
+    Table, 
+    TableHead, 
+    TableRow, 
+    TableCell, 
+    TableBody 
+} from "@material-ui/core";
 
 const AssetTableCard = () => {
     return(
         <Card elevation={6} className="px-24 pt-20 py-16 mb-24">
             <div className="card-title ml-12">Your Assets</div>
-            Table Here
+            <div>
+                <Table>
+                    <TableHead>
+                        <TableCell>Asset</TableCell>
+                        <TableCell>Balance</TableCell>
+                        <TableCell>Allocation</TableCell>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>asset name</TableCell>
+                            <TableCell>balance value</TableCell>
+                            <TableCell>allocation percentage</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
         </Card>
     );
 };
