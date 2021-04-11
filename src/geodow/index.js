cca5b79119c057d28a2e8c8f13ead867acac47cc
George <georgedowne06@gmail.com>
2021-04-11 16:11:58 +0100

    Added SimpleCard component and updated the dashboard component

--- src/geodow/index.js
+++ src/geodow/index.js
@@ -1,3 +1,5 @@
 export {
     default as GeodowLoadable
 } from "./components/GeodowLoadable/GeodowLoadable";
+
+export { default as SimpleCard } from "./components/cards/SimpleCard";
--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,27 +1,19 @@
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { logoutUser } from "redux/actions/UserActions";
 import React, { Component } from 'react';
-import { withStyles, Button, Card, Grid } from "@material-ui/core";
+import { withStyles, Card, Grid } from "@material-ui/core";
 import { withRouter } from "react-router-dom";
+import { Fragment } from "react";
+import { SimpleCard } from "geodow";
 
 class Dashboard extends Component {
     
     render() {
+        let { theme } = this.props;
+
         return (
-            <div className="dashboard flex flex-center w-100 h-100vh-160">
-                <div className="p-4">
-                    <Card className="position-relative y-center">
-                        <Grid container spacing={3}>
-                            <Grid item lg={8} md={8} sm={12} xs={12}>
-                                Welcome to the Dashboard
-                            </Grid>
-                            <Grid item lg={4} md={4} sm={12} xs={12}>
-                                <Button onClick={() => this.props.logoutUser()}>Log out</Button>
-                            </Grid>
-                        </Grid>
-                    </Card>
-                </div>
+            <div className="dashboard m-sm-30 ">
+                <SimpleCard title="dashboard"></SimpleCard>
             </div>
         )
     }
@@ -37,8 +29,5 @@ const mapStateToProps = state => ({
     user: state.user
 });
 
-const mapDispatchToProps = dispatch => ({
-    logoutUser: () => dispatch(logoutUser())
-})
 
-export default withStyles({}, {withTheme: true})(withRouter(connect(mapStateToProps, mapDispatchToProps)(Dashboard)));
+export default withStyles({}, {withTheme: true})(withRouter(connect(mapStateToProps, {})(Dashboard)));
