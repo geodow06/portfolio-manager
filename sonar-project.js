a26ea0491b4e56e3984e1daf0042b6ed042ac4da
George <georgedowne06@gmail.com>
2021-08-05 17:32:16 +0100

    Adding sonar scanner config and scripts

---
+++ sonar-project.js
@@ -0,0 +1,10 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+// import sonarqubeScanner from "sonarqube-scanner";
+
+sonarqubeScanner({
+  serverUrl: 'http://localhost:9000',
+  options : {
+  'sonar.sources': '.',
+  'sonar.inclusions' : 'src/**' // Entry point of your code
+  }
+}, () => {});
