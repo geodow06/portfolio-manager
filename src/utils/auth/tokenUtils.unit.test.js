eab3ec98ea553e24ef78956b7269a32fa9624492
George <georgedowne06@gmail.com>
2021-08-05 22:16:30 +0100

    Added tokenUtils tests

--- src/utils/auth/tokenUtils.unit.test.js
+++ src/utils/auth/tokenUtils.unit.test.js
@@ -1,39 +1,40 @@
-import { jwkArrayToObject, base64DecodeJSON } from "utils";
+import { 
+    jwkArrayToObject, 
+    base64DecodeJSON, 
+    decodeJWTHeaderAndPayload 
+} from "utils";
+import * as constants from "utils/auth/constants";
 import chai from 'chai';
 
-const dummyJWKArray = [
-    {
-        kid: "kid1", 
-        item1: "item1",
-        item2: "item2"
-    },
-    {
-        kid: "kid2", 
-        item3: "item3",
-        item4: "item4"
-    }
-];
-const correctlyFormattedJWKObject = {
-    "kid1": {
-        item1: "item1",
-        item2: "item2"
-    }, 
-    "kid2":{
-        item3: "item3",
-        item4: "item4"
-    }
-};
-
 describe('tokenUtils unit tests', () => {
+    // formatJWKArray tests
     describe('formatJWKArray tests', () => {
         it('Should correctly format JWK array to object', () => {
-            chai.expect(jwkArrayToObject(dummyJWKArray)).to.deep.equal(correctlyFormattedJWKObject);
+            chai.expect(jwkArrayToObject(constants.testJWKArray)).to.deep.equal(constants.testCorrectlyFormattedJWKObject);
         });
     });
-    describe('base64DecodeJSON test', () => {
+
+    // base64DecodeJSON tests
+    describe('base64DecodeJSON tests', () => {
         it('Should return empty object for malformed JSON', () => {
-            const emptyObject = {};
-            chai.expect(base64DecodeJSON(emptyObject)).to.be.empty;
+            chai.expect(base64DecodeJSON(constants.malformedBase64EncodedJSON)).to.be.empty;
+        });
+        it('Should return object decoded object', () => {
+            chai.expect(base64DecodeJSON(constants.test64EncodedJSON)).to.deep.equal(constants.test64DecodedJSON);
+        });
+    });
+
+    // signToken tests
+    describe('decodeJWTHeaderAndPayload tests', () => {
+        const decodedToken = decodeJWTHeaderAndPayload(constants.testJWT);
+        it('Should return oject with header and payload props', () => {
+            chai.expect(decodedToken).to.have.keys('header', 'payload');
+        });
+        it('Should return an object with the JWT header correctly decoded', () => {
+            chai.expect(decodedToken.header).to.include(constants.testDecodedJWTHeader);
+        });
+        it('Should return an object with the JWT payload correctly decoded', () => {
+            chai.expect(decodedToken.payload).to.include(constants.testDecodedJWTPayload);
         });
     });
 });
